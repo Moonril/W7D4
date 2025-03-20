7e30ea9caedd8b0980f4836886b456bc9b401b33
Vincenza <vinni2690@hotmail.com>
2025-03-20 22:31:55 +0100

    adds modals + avg colors

--- script/pexels-start.js
+++ script/pexels-start.js
@@ -11,9 +11,9 @@ const imgsID = document.querySelectorAll('.text-muted')
 
 //montagne
 
-const getMountainsImgs = function () {
+const getMountainsImgs = function (query) {
 
-    fetch(mountainsImgURL, {
+    fetch('https://api.pexels.com/v1/search?query=' + query, {
         headers: {
             Authorization: 'PA3kOW0Sflfbq1yPCWPFkA7G25eRlKqJMLlfBbwmLoUxVl73IeV6p4X0'
         }
@@ -44,7 +44,7 @@ const getMountainsImgs = function () {
         for (let i = 0; i < numImages; i++) {
             const photo = mountains.photos[i]
             
-            imgsHtml[i].src = photo.src.small
+            imgsHtml[i].src = photo.src.large
             imgsID[i].innerText =  photo.id
         }
 
@@ -60,9 +60,9 @@ const getMountainsImgs = function () {
 }
 
 //CIPOLLE
-const getCipolleImgs = function () {
+const getCipolleImgs = function (query) {
 
-    fetch(onionsImgURL, {
+    fetch('https://api.pexels.com/v1/search?query=' + query, {
         headers: {
             Authorization: 'PA3kOW0Sflfbq1yPCWPFkA7G25eRlKqJMLlfBbwmLoUxVl73IeV6p4X0'
         }
@@ -92,7 +92,7 @@ const getCipolleImgs = function () {
 
         for (let i = 0; i < numImages; i++) {
             const photo = mountains.photos[i]
-            imgsHtml[i].src = photo.src.small
+            imgsHtml[i].src = photo.src.large
             imgsID[i].innerText =  photo.id
             
         }
@@ -113,7 +113,7 @@ const getCipolleImgs = function () {
 const loadMountains = document.getElementById('load-button')
 
 loadMountains.addEventListener('click', function(){
-    getMountainsImgs()
+    getMountainsImgs('mountains')
 
 })
 
@@ -122,7 +122,7 @@ loadMountains.addEventListener('click', function(){
 const loadCipolle = document.getElementById('cipolla-button')
 
 loadCipolle.addEventListener('click', function(){
-    getCipolleImgs()
+    getCipolleImgs('onions')
 
 })
 
@@ -189,7 +189,7 @@ const loadImgs = function (query) {
         for (let i = 0; i < numImages; i++) {
             const photo = mountains.photos[i]
             
-            imgsHtml[i].src = photo.src.small
+            imgsHtml[i].src = photo.src.large
             imgsID[i].innerText =  photo.id
         }
 
@@ -210,6 +210,75 @@ customButton.addEventListener('click', function() {
 })
 // 7) Cliccare l'immagine o il suo nome farà cambiare pagina verso una di dettaglio dell'immagine. Qui dovrai visualizzare immagine, nome artista e linkare la sua pagina personale. Dai la possibilità all'utente di tornare indietro come esercizio fatto giovedì in classe
 
-// quando clicchi view, ti apre un modale con l'immagine più grande
+const getAvgColors = function (query) {
+
+    fetch('https://api.pexels.com/v1/search?query=' + query, {
+        headers: {
+            Authorization: 'PA3kOW0Sflfbq1yPCWPFkA7G25eRlKqJMLlfBbwmLoUxVl73IeV6p4X0'
+        }
+    })
+    .then((response) => {
+        if(response.ok){
+            return response.json()
+        } else {
+            throw new Error('errore prima parte, not ok')
+        }
+    })
+    .then((colors) => {
+        console.log('mountains', colors)
+        // è un oggetto contentente un array di 15 oggetti.
+        // mountains.photos[i].url oppure .src.small .portrait .medium
+        // ora bisogna assegnare ogni singola foto alle card che esistono già
+
+        // document.getElementById("imageid").src="../template/save.png";
+
+
+        if (!colors.photos || colors.photos.length === 0) {
+            throw new Error('Nessuna foto disponibile');
+        }
+
+
+        const numImages = Math.min(imgsHtml.length, colors.photos.length)
+
+        for (let i = 0; i < numImages; i++) {
+            //const photo = mountains.photos[i]
+            const modalBody = document.getElementById('modal-body')
+            const avgBG = colors.photos[i].avg_color
+
+            modalBody.style.backgroundColor = avgBG
+        }
+
+
+    })
+    .catch((err) => {
+        console.log('si è verificato un errore', err)
+    })
+
+
+
+
+}
+
+
+// quando clicchi view, ti apre un modale con l'immagine più grande + avg color
+const viewBtns = document.querySelectorAll('.view')
+
+viewBtns.forEach(btn => {
+    btn.addEventListener('click', function (e){
+        //quando clicco questo la src dll'img cliccata deve andare alla src del modale
+        const modalPicture = document.querySelector('.modal-img')
+        modalPicture.src = e.target.closest('.card').querySelector('img').src
+        getAvgColors('beach')
+        
+    })
+})
+
+
 // ogni img ha un avg_color, metterla come sfondo al modale
+
+
+
+
+
+
 // repo stefano https://github.com/irvelervel/FS0125-U2-W3-D4-HW
